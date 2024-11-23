c5037ebc8f09f66188e52495bb4a251dee68765e
KabAyrat <kabirov.ayrat2004@gmail.com>
2024-11-23 08:52:56 +0300

    auth_form_frontend

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+const loginBtn = document.getElementById("login-btn")
+const registerBtn = document.getElementById("register-btn")
+const loginForm = document.getElementById("login-form")
+const registerForm = document.getElementById("register-form") 
+
+loginBtn.addEventListener('click', function(){
+    loginBtn.classList.add('active')
+    registerBtn.classList.remove('active')
+    loginForm.classList.add('active')
+    registerForm.classList.remove('active')
+})
+registerBtn.addEventListener('click', function(){
+    registerBtn.classList.add('active')
+    loginBtn.classList.remove('active')
+    registerForm.classList.add('active')
+    loginForm.classList.remove('active')
+})
